4c43e12360ec156a6ff6877dddd6a97859199d1f
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-01-17 11:54:03 +0600

    Fixup test

--- test/main.js
+++ test/main.js
@@ -15,6 +15,7 @@ describe('hashdir', function () {
             assert.equal(result['index.js'], 'a77b15c0d64cf6111e025645e562faf8ccabb9d7');
             assert.strictEqual(result.emptyfolder, undefined);
             assert.equal(result['subfolder/module.js'], '52c32688290d33191d4f56c9066eb4cb14daf60c');
+            assert.strictEqual(result.subfolder, undefined);
 
             done();
         });
